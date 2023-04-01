23ba9a7843ec6fa4625871453c14255ef6f84d9c
rahimtech <rahimianalytic@gmail.com>
2023-04-01 15:42:45 +0430

    Version 4

---
+++ src/components/Main.jsx
@@ -0,0 +1,369 @@
+import React, { useState, memo } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { Column } from "./Column";
+import { v4 as uuid } from "uuid";
+// import "../../css/app.css";
+import "../css/output.css";
+import { useEffect } from "react";
+import UserContext from "./Context/UserContext";
+import Nav from "./Navigation/Nav";
+import Axios from "axios";
+import { json } from "react-router-dom";
+import { Logout } from "@mui/icons-material";
+
+const itemsFromBackend = [
+  {
+    id: uuid(),
+    content: "اولین کارتو بنویس !",
+    color: "",
+    sizeA: "",
+    boardId: "B1",
+  },
+  // { id: uuid(), content: "Fifth task", color: "red" },
+];
+
+const columnsFromBackend = {
+  c1: {
+    id: uuid(),
+    name: "ToDo",
+    items: itemsFromBackend,
+    boardId: "B1",
+  },
+  c2: {
+    id: uuid(),
+    name: "Doing",
+    items: [],
+    boardId: "B1",
+  },
+  c3: {
+    id: uuid(),
+    name: "Done",
+    items: [],
+    boardId: "B1",
+  },
+};
+
+const mainBoard = [
+  {
+    nameBoard: "تخته اول",
+    idBoard: "B1",
+  },
+  {
+    nameBoard: "تخته دوم",
+    idBoard: "B2",
+  },
+];
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination, type, droppableId, draggableId } = result;
+
+  if (type === "column") {
+    var result = Object.values(columns);
+    // var result2 = Array.form(columns);
+
+    const [rev] = result.splice(source.index, 1);
+
+    result.splice(destination.index, 0, rev);
+
+    const newState = {
+      ...result,
+    };
+
+    setColumns(newState);
+    // console.log("columns", Object.keys(columns));
+
+    // const columnOrder = Object.keys(columns);
+    // const newColumnOrder = Array.from(columnOrder);
+    // console.log("newColumnOrder", newColumnOrder);
+
+    // newColumnOrder.splice(source.index, 1);
+    // newColumnOrder.splice(destination.index, 0, draggableId);
+    // console.log("newColumnOrder2", newColumnOrder);
+
+    // const newState = {
+    //   ...columns
+    // };
+    // console.log("newState", newState);
+
+    // setColumns(newState);
+
+    return;
+  }
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
+
+const onDragStart = (result) => {
+  if (window.navigator.vibrate) {
+    window.navigator.vibrate(100);
+  }
+};
+
+function Main() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+  const [columnIdSend, setColumnId] = useState();
+  const [board, setMainBoard] = useState(mainBoard[0].idBoard);
+  const [color, setColor] = useState("red");
+  const [colorback, setColorBack] = useState("!bg-blue-500");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  // const [columnBtnId, setcolumnBtnId] = useState({
+  //   item: "",
+  //   columnIDD: "",
+  // });
+  const [sizeArea, setSizeArea] = useState("");
+
+  // useEffect(() => {
+  //   const getColumns = JSON.parse(localStorage.getItem("columns"));
+  //   const getBackColor = JSON.parse(localStorage.getItem("colorback"));
+  //   if (getColumns || getBackColor) {
+  //     setColumns(getColumns);
+  //     setColorBack(getBackColor);
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("columns", JSON.stringify(columns));
+  //   localStorage.setItem("colorback", JSON.stringify(colorback));
+  // }, [columns, colorback]);
+
+  const addTask = (task, columnId) => {
+    // setcolumnBtnId({
+    //   item: task,
+    //   columnIDD: columnId,
+    // });
+    const newColumn = [task, ...columns[columnId].items];
+
+    const orderColumn = {
+      ...columns,
+      [columnId]: {
+        name: columns[columnId].name,
+        items: newColumn,
+        boardId: board,
+      },
+    };
+
+    setColumns(orderColumn);
+  };
+
+  const addColumn = (addCol) => {
+    const newColumn = { ...columns, [uuid()]: addCol };
+
+    setColumns(newColumn);
+  };
+
+  const delTask = (taskId, columnId) => {
+    const updateTask = columns[columnId].items.filter(
+      (item) => item.id != taskId
+    );
+    columns[columnId].items = updateTask;
+
+    const updateColumn = { ...columns };
+
+    setColumns(updateColumn);
+  };
+
+  const delColumn = (columnId, objId) => {
+    delete columns[`${objId}`];
+    setColumns({ ...columns });
+  };
+
+  const setcc = (taskId, columnId, myColor) => {
+    columns[columnId].items[taskId].color = myColor;
+
+    const ArrayCol = Object.values(columns);
+
+    setColumns({ ...columns });
+  };
+
+  const sendTask = (e) => {
+    if (e) e.preventDefault();
+
+    Axios.post("http://localhost:3001/tasksend", {
+      email: email,
+      taskTitle: JSON.stringify(columns),
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  const InnerList = memo((props) => {
+    // console.log("this.props;: ", props);
+
+    // const tasks = column.taskIds.map((taskId) => taskMap[taskId]);
+    return (
+      <div>
+        <Column
+          column={props.column}
+          columnId={props.columnId}
+          index={props.index}
+          colId={props.column.id}
+        />
+      </div>
+    );
+  });
+  // return <Column column={column} tasks={tasks} index={index} />;
+
+  const logOutUser = () => {
+    Axios.post("http://localhost:3001/logout", {
+      email: email,
+      taskTitle: JSON.stringify(columns),
+    }).then((response) => {
+      console.log("response: ", response.data[0].taskCheckUser);
+    });
+  };
+
+  function logInUser() {
+    // if (e) e.preventDefault();
+    // Axios.get("http://localhost:3001/checklogin", {
+    //   email: email,
+    // })
+    //   .then((response) => {
+    //     console.log("response: ", response.data[0].taskCheckUser);
+    //     return true;
+    //   })
+    //   .catch(() => {
+    //     return false;
+    //   });
+  }
+  return (
+    <div className={`${colorback} min-h-screen overflow-auto `}>
+      <UserContext.Provider
+        value={{
+          columns,
+          setColumns,
+          color,
+          setColor,
+          colorback,
+          setColorBack,
+          board,
+          setMainBoard,
+          // columnBtnId,
+          // setcolumnBtnId,
+          setcc,
+          delTask,
+          delColumn,
+          addTask,
+          sendTask,
+          username,
+          setUsername,
+          email,
+          setEmail,
+          logOutUser,
+          logInUser,
+          columnIdSend,
+          mainBoard,
+        }}
+      >
+        <Nav />
+        {/* <button onClick={logOutUser}>out</button> */}
+        {/* <div className="bg-yellow-300 w-full">
+          <hr className="mt-5 " />
+          <h3 className="text-center">
+            این پروژه در حال ساخت است و تکمیل نیست آخرین آپدیت 14 دی 1401
+          </h3>
+          <hr className="mb-5 " />
+        </div>
+        <div className="bg-green-300 w-full">
+          <hr className="mt-5 " />
+          <h4 className="text-center mt-2">
+            موارد پر استفاده و مهم در پروژه ترلو :
+          </h4>
+          <h5 className="text-center ">
+            Programming: JavaScript / FrameWork: React / Dragging:
+            React-Beautiful-dnd / CSS: Tailwind / Design: Mui
+          </h5>
+          <hr className="mb-5 " />
+        </div> */}
+
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+          onDragStart={(result) => onDragStart(result)}
+          // onDragUpdate={(result) => onDragStart(result)}
+        >
+          <Droppable
+            droppableId="all-columns"
+            direction="horizontal"
+            type="column"
+          >
+            {(provided) => {
+              return (
+                <div
+                  style={{ display: "flex" }}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="md:mt-24"
+                >
+                  {Object.entries(columns).map(([columnId, column], index) => {
+                    if (column.boardId === board) {
+                      setColumnId(columnId);
+                      return (
+                        <InnerList
+                          key={columnId}
+                          columnId={columnId}
+                          column={column}
+                          index={index}
+                        />
+                      );
+                    }
+                  })}
+
+                  {provided.placeholder}
+                  <div className=" !p-7 ">
+                    <button
+                      className="w-40 mt-3 h-11 rounded-md border-none shadow-lg outline-none cursor-pointer"
+                      onClick={() =>
+                        addColumn({
+                          id: uuid(),
+                          name: "ستون جدید",
+                          items: [],
+                          boardId: board,
+                        })
+                      }
+                    >
+                      ستون جدید
+                    </button>
+                  </div>
+                </div>
+              );
+            }}
+          </Droppable>
+        </DragDropContext>
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/components/Navigation/Board.jsx
@@ -0,0 +1,170 @@
+import React, { useState, memo } from "react";
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { v4 as uuid } from "uuid";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Typography } from "@mui/material";
+import { useContext } from "react";
+import UserContext from "../Context/UserContext";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
+
+export default function Board() {
+  const con = useContext(UserContext);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [nameBoardShow, setNameBoardShow] = useState(
+    `${con.mainBoard[0].nameBoard}`
+  );
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (setBoardId, setBoardName) => {
+    if (setBoardId === null) {
+    } else {
+      setNameBoardShow(setBoardName);
+      con.setMainBoard(setBoardId);
+
+      // con.setColumns({ ...con.columns });
+    }
+    setAnchorEl(null);
+  };
+
+  const delBoard = (id) => {
+    if (con.mainBoard.length <= 1) {
+      return;
+    }
+    con.mainBoard.find((item, index) => {
+      if (item.idBoard == id) {
+        const remv = con.mainBoard.indexOf(con.mainBoard[index]);
+        if (remv > -1) {
+          con.mainBoard.splice(remv, 1);
+
+          setNameBoardShow(con.mainBoard[0].nameBoard);
+          con.setMainBoard(item.idBoard);
+        }
+      }
+    });
+  };
+
+  const addBoard = (setIdBoard, setNameNewBoard) => {
+    const endNameBoard = setNameNewBoard + con.mainBoard.length;
+    con.mainBoard.push({
+      nameBoard: endNameBoard,
+      idBoard: setIdBoard,
+    });
+    con.setMainBoard(setIdBoard);
+    setNameBoardShow(endNameBoard);
+  };
+
+  const InnerList = memo(({ item, index }) => {
+    return (
+      <div className="flex items-center">
+        <DeleteIcon
+          className="cursor-pointer z-10"
+          color="error"
+          fontSize="small"
+          onClick={() => delBoard(item[1].idBoard)}
+        />
+        <MenuItem
+          className="w-full"
+          key={index}
+          onClick={() =>
+            handleClose(`${item[1].idBoard}`, `${item[1].nameBoard}`)
+          }
+          disableRipple
+        >
+          <Typography>{item[1].nameBoard}</Typography>
+        </MenuItem>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? "demo-customized-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        {nameBoardShow}
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          "aria-labelledby": "demo-customized-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => handleClose(null)}
+        className="rtl"
+      >
+        {Object.entries(con.mainBoard).map((item, index) => {
+          return <InnerList item={item} index={index} />;
+        })}
+
+        <MenuItem
+          className="text-center"
+          onClick={() => addBoard(uuid(), `تخته جدید `)}
+          disableRipple
+        >
+          <AddIcon fontSize="medium" />
+          <Typography>افزودن</Typography>
+        </MenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
